c6a2d6b8338bf776b98e9a301264d7b89319b691
Zamzam Raza <122750078+Zamzam1707@users.noreply.github.com>
2023-06-11 15:50:17 +0530

    data types

---
+++ Q1/variable.js
@@ -0,0 +1,36 @@
+let x= 10;
+var y = 20;
+const z = 'pooja';
+
+//let, var, const- are three ways to declare variable.
+
+//data types
+
+
+// 1. numbers
+var num= 7;
+let num1 = 10;
+console.log(num1);
+
+//2. string
+
+const names= ["gulab", "mogra", "chameli", "lotus"];
+console.log(names);
+
+//3. boolean
+
+var loginfromgoggle = true;
+var loginfromfacebook = false;
+console.log(loginfromfacebook);
+
+//4.null- means thing stored inside or null
+
+let random = null;
+console.log(random);
+
+//5. undefined
+
+var paymentmode;
+
+console.log(paymentmode);
+paymentmode = "credit card";
