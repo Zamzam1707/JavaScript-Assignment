fa073307bbf40adbfa00fce76e6505ba511ee658
Zamzam Raza <122750078+Zamzam1707@users.noreply.github.com>
2023-06-11 16:06:32 +0530

    editing array

---
+++ Q20/scripet.js
@@ -0,0 +1,11 @@
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+ shoppingCart.unshift('Meat');
+
+
+shoppingCart.push('sugar');
+
+shoppingCart.splice(3, 1, 'Green Tea');
+
+shoppingCart.splice(4, 1);
+
+console.log(shoppingCart);
